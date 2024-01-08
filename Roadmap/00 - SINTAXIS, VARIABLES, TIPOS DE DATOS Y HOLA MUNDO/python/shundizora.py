77b291fede3c9ffbbb989f9d2616eb40a274048a
Willian Quintero <wrquinterom@unal.edu.co>
2024-01-08 13:41:13 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/shundizora.py
@@ -0,0 +1,12 @@
+#Así se realiza un comentario
+#link de este lenguaje de programción: https://es.python.org/
+
+cadena = "Cadena de texto"
+numeroEntero = 10 #Constante entera
+numeroNegativo = -10 #Constante Negativa
+numeroDecimal = 5.5 #Constante decimal
+variableDependiente = numeroDecimal #Variable dependiente
+lenguaje = "Python" #Declarar Lenguaje de programación para pimprimirlo luego
+boolean = False    #Booleano
+#Imprimir el primer Hola + lenguaje de programación
+print("¡Hola, " + lenguaje + "!")
