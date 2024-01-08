adab36773b536a1387f5b5e6ab7aa56e2a050a87
Willian Quintero <wrquinterom@unal.edu.co>
2024-01-08 13:40:46 -0500

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/shundizora.js
@@ -0,0 +1,15 @@
+//Así se realiza un comentario
+//link de este lenguaje de programción: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+
+
+const letra = 'a'; //Caracter debe estar solo entre ''
+let cadena = "Cadena de texto"; //texto completo debe estar entre ""
+let numeroEntero = 10; //Constante entera
+let numeroNegativo = -10; //Constante Negativa
+let numeroDecimal = 5.5; //Constante decimal
+let variableDependiente = numeroDecimal; //Variable dependiente
+let lenguaje = "C++"; //Declarar Lenguaje de programación para pimprimirlo luego
+let boolean = false;   //Booleano
+//Imprimir el primer Hola + lenguaje de programación
+console.log('Hola, '+lenguaje+'!');
