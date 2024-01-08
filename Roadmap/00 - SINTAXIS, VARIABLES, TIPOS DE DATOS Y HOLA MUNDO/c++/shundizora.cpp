9d353fc2b5cdadddd9616104de9a1477919d1e5f
Willian Quintero <wrquinterom@unal.edu.co>
2024-01-08 13:39:50 -0500

    #00 - C++

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c++/shundizora.cpp
@@ -0,0 +1,24 @@
+//Así se realiza un comentario
+//link de este lenguaje de programción: https://isocpp.org
+
+//Se importa la biblioteca iostream
+#include <iostream>
+// Acorta los comandos de entrada y salida de consola
+using namespace std;
+
+int main(){
+
+char letra = 'a'; //Caracter debe estar solo entre ''
+string cadena = "Cadena de texto"; //texto completo debe estar entre ""
+int numeroEntero = 10; //Constante entera
+double numeroNegativo = -10; //Constante Negativa
+float numeroDecimal = 5.5; //Constante decimal
+float variableDependiente = numeroDecimal; //Variable dependiente
+string lenguaje = "C++"; //Declarar Lenguaje de programación para pimprimirlo luego
+bool boolean = false;   //Booleano
+//Imprimir el primer Hola + lenguaje de programación
+cout <<"¡Hola, "<<lenguaje<<"!"<<endl;
+
+return 0;
+
+}
